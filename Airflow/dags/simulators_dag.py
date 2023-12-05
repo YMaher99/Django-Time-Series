a14f8d01fa054599fd151a483c2ef198057e6d51
YMaher99 <youssefmaher725@gmail.com>
2023-12-05 02:55:34 +0200

    changed how Airflow generates the datasets

--- Airflow/dags/simulators_dag.py
+++ Airflow/dags/simulators_dag.py
@@ -10,15 +10,37 @@ sys.path.append('/app')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simulator.settings")
 django.setup()
 
+from configurers.django_configuration_manager import DjangoConfigurationManager
+from generators.coefficients_generator import CoefficientsGenerator
+from producers.csv_data_producer import CSVDataProducer
+from producers.nifi_producer import NIFIProducer
+from producers.kafka_producer import KafkaProducer
+from simulator_operations.parallel_run_simulator import ParallelRunSimulator
 from simulator_api.models import Simulator
 
 
 def run_simulator(pk, **kwargs):
 
-    path = f"http://web:8000/simulator/{pk}/run_simulator"
-    response = requests.get(url=path)
+    instance = Simulator.objects.get(pk=pk)
+    config_manager = DjangoConfigurationManager(instance)
+    generator = CoefficientsGenerator(config_manager)
+    if instance.producer_type == instance.CSV:
+        producer = CSVDataProducer(config_manager)
+    elif instance.producer_type == instance.KAFKA:
+        producer = KafkaProducer(config_manager)
+    elif instance.producer_type == instance.NIFI:
+        producer = NIFIProducer(config_manager)
+    else:
+        producer = KafkaProducer(config_manager)
+
+    if instance.simulator_runner is None:
+        instance.simulator_runner = ParallelRunSimulator(config_manager, generator, producer)
+    instance.simulator_runner.run_simulator()
+
+    # path = f"http://web:8000/simulator/{pk}/run_simulator"
+    # response = requests.get(url=path)
     ti = kwargs['ti']
-    ti.xcom_push(key='status_code', value=response.status_code)
+    ti.xcom_push(key='status_code', value=200)
 
 
 def check_simulator_status(**kwargs):
